52e02606e3e5cd8cbdb60cb06081b24b5c1a323c
olina6 <olina6@gmail.com>
2021-09-24 15:15:10 +1200

    complete computed of total price and the red icon shows how many items in cart

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -6,10 +6,10 @@
           src="http://www.dell-lee.com/imgs/vue3/basket.png"
           class="check_icon_img"
         />
-        <div class="check_icon_tag">1</div>
+        <div class="check_icon_tag">{{total}}</div>
       </div>
       <div class="check_info">
-        Total: <span class="check_info_price">&#36;127</span>
+        Total: <span class="check_info_price">&#36;{{ price }}</span>
       </div>
       <div class="check_btn">
         Checkout
@@ -17,9 +17,53 @@
     </div>
   </div>
 </template>
+
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+// get cart information
+const useCartEffect = () => {
+  const store = useStore()
+  const route = useRoute()
+  const shopId = route.params.id
+  const cartList = store.state.cartList
+  const total = computed(() => {
+    const productList = cartList[shopId]
+    let count = 0
+    if (productList) {
+      for (const i in productList) {
+        const product = productList[i] // get current product
+        count += product.count
+      }
+    }
+    return count
+  })
+  const price = computed(() => {
+    const productList = cartList[shopId]
+    let count = 0
+    if (productList) {
+      for (const i in productList) {
+        const product = productList[i]
+        count += (product.count * product.price)
+      }
+    }
+    return count.toFixed(2)
+  })
+  return { total, price }
+}
+
+export default {
+  name: 'Cart',
+  setup () {
+    const { total, price } = useCartEffect()
+    return { total, price }
+  }
+}
 
 </script>
+
 <style lang="scss" scoped>
 @import "src/style/variables.scss";
 .cart{
@@ -45,7 +89,7 @@
     &_tag{
       position: absolute;
       left: .15rem;
-      top:.07rem;
+      top:.04rem;
       width: .2rem;
       height: .2rem;
       line-height: .2rem;
@@ -54,14 +98,16 @@
       font-size: .12rem;
       text-align: center;
       color: #fff;
-      transform: scale(.5);
+      transform: scale(.65);
     }
   }
   &_info{
+    margin: .05rem;
     flex:1;
     color: $content-fontcolor;
     font-size: .12rem;
     &_price{
+      padding: .03rem;
       color: $highlight-fontColor;
     }
   }
